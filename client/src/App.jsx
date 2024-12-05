067a4d38febb58da7d836a624db913378a965003
kevin kalenga <kevinkalenga10@gmail.com>
2024-12-05 13:49:39 +0100

    Complete update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import About from './pages/About';
 import CreateListing from './pages/createListing';
+import UpdateListing from './pages/UpdateListing';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -21,6 +22,7 @@ function App() {
            <Route path="" element={<PrivateRoute />}>
                 <Route path='/profile' element={<Profile />} />
                 <Route path='/create-listing' element={<CreateListing />} />
+                <Route path='/update-listing/:listingId' element={<UpdateListing />} />
            </Route>
        
        </Routes>
